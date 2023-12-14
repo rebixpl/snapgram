09523a93fa3c2eb3b97c2c2b8ccfbeedaddd87b3
rebixpl <33410494+rebixpl@users.noreply.github.com>
2023-12-14 15:06:41 +0100

    auth pages update 1 + assets import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { Routes, Route } from "react-router-dom";
 import "./globals.css";
+import SignInForm from "./_auth/forms/SignInForm";
+import { Home } from "./_root/pages";
+import SignUpForm from "./_auth/forms/SignUpForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello Snapgram!</h1>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes like signup and login */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
+        {/* private routes like dashboard and settings */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
