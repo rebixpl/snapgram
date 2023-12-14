a12ac6be1c86f4b0b5d4447ae083b0cd87a0936f
rebixpl <33410494+rebixpl@users.noreply.github.com>
2023-12-14 20:14:39 +0100

    auth pages, sign up page

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,10 @@
+import * as z from "zod";
+
+export const SignupValidation = z.object({
+  name: z.string().min(2, { message: "Name is too short!" }),
+  username: z.string().min(2, { message: "Username is too short!" }),
+  email: z.string().email({ message: "Invalid email address!" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters!" }),
+});
