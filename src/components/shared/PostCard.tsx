7831baf8aa02c1d8e5b2d7b59a01b4abc309d550
rebixpl <33410494+rebixpl@users.noreply.github.com>
2023-12-18 16:41:33 +0100

    bugfix with tags not displaying properly on mobile

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -19,26 +19,23 @@ const PostCard = ({ post }: PostCardProps) => {
           <Link to={`/profile/${post.creator.$id}`}>
             <img
               src={
-                post?.creator?.imageUrl ||
+                post.creator?.imageUrl ||
                 "/assets/icons/profile-placeholder.svg"
               }
               alt="creator"
-              className="rounded-full w-12 lg:h-12"
+              className="w-12 lg:h-12 rounded-full"
             />
           </Link>
 
-          <div className="flex flex-col ">
-            <p
-              className="base-medium lg:body-bold 
-            text-light-1"
-            >
+          <div className="flex flex-col">
+            <p className="base-medium lg:body-bold text-light-1">
               {post.creator.name}
             </p>
             <div className="flex-center gap-2 text-light-3">
-              <p className="subtle-semibold lg:small-regular">
+              <p className="subtle-semibold lg:small-regular ">
                 {multiFormatDateString(post.$createdAt)}
               </p>
-              -
+              •
               <p className="subtle-semibold lg:small-regular">
                 {post.location}
               </p>
@@ -50,16 +47,21 @@ const PostCard = ({ post }: PostCardProps) => {
           to={`/update-post/${post.$id}`}
           className={`${user.id !== post.creator.$id && "hidden"}`}
         >
-          <img src="/assets/icons/edit.svg" alt="edit" width={20} height={20} />
+          <img
+            src={"/assets/icons/edit.svg"}
+            alt="edit"
+            width={20}
+            height={20}
+          />
         </Link>
       </div>
 
       <Link to={`/posts/${post.$id}`}>
         <div className="small-medium lg:base-medium py-5">
           <p>{post.caption}</p>
-          <ul className="flex gap-1 mt-2">
-            {post.tags.map((tag: string) => (
-              <li key={tag} className="text-light-3">
+          <ul className="flex flex-wrap gap-1 mt-2">
+            {post.tags.map((tag: string, index: number) => (
+              <li key={`${tag}${index}`} className="text-light-3 small-regular">
                 #{tag}
               </li>
             ))}
