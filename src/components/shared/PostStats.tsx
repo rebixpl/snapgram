ab9454183fa0a7c6ec792823b111bb97f202b2dc
rebixpl <33410494+rebixpl@users.noreply.github.com>
2023-12-18 17:57:13 +0100

    implemented logic behind (likes, save) on the home page posts cards #2

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,4 +1,12 @@
+import { useUserContext } from "@/context/AuthContext";
+import {
+  useDeleteSavePost,
+  useLikePost,
+  useSavePost,
+} from "@/lib/react-query/queriesAndMutations";
+import { checkIsLiked } from "@/lib/utils";
 import { Models } from "appwrite";
+import React, { useState } from "react";
 
 type PostStatsProps = {
   post: Models.Document;
@@ -6,6 +14,56 @@ type PostStatsProps = {
 };
 
 const PostStats = ({ post, userId }: PostStatsProps) => {
+  const likesList = post.likes.map((user: Models.Document) => user.$id);
+
+  const [likes, setLikes] = useState(likesList);
+  const [isSaved, setIsSaved] = useState(false);
+
+  const { mutate: likePost } = useLikePost();
+  const { mutate: savePost } = useSavePost();
+  const { mutate: deleteSavedPost } = useDeleteSavePost();
+
+  //   const { data: currentUser } = useGetCurrentUser();
+
+  const handleLikePost = (e: React.MouseEvent) => {
+    e.stopPropagation(); // this is to prevent the click
+    // event from bubbling up to the parent element
+
+    let newLikes = [...likes];
+
+    const hasLiked = newLikes.includes(userId);
+
+    if (hasLiked) {
+      // remove the like
+      newLikes = newLikes.filter((id) => id !== userId);
+    } else {
+      // add the like
+      newLikes.push(userId);
+    }
+
+    setLikes(newLikes);
+    likePost({ postId: post.$id, likesArray: newLikes });
+  };
+
+  const handleSavePost = (e: React.MouseEvent) => {
+    e.stopPropagation(); // this is to prevent the click
+    // event from bubbling up to the parent element
+
+    const savedPostRecord = currentUser?.save.find(
+      (record: Models.Document) => record.$id === post.$id
+    );
+
+    if (savedPostRecord) {
+      // delete the saved post
+      setIsSaved(false);
+      deleteSavedPost(savedPostRecord.$id);
+    } else {
+      // save the post
+      savePost({ postId: post.$id, userId });
+      setIsSaved(true);
+    }
+  };
+
   return (
     <div
       className="flex justify-between i
@@ -13,22 +71,28 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
     >
       <div className="flex gap-2 mr-5">
         <img
-          src="/assets/icons/like.svg"
+          src={`${
+            checkIsLiked(likes, userId)
+              ? "/assets/icons/liked.svg"
+              : "/assets/icons/like.svg"
+          }`}
           alt="like"
           width={20}
           height={20}
-          onClick={() => {}}
+          // onClick={(e) => handleLikePost(e)}
+          // this is the same as below
+          onClick={handleLikePost}
           className="cursor-pointer"
         />
-        <p className="small-medium lg:base-medium">0</p>
+        <p className="small-medium lg:base-medium">{likes.length}</p>
       </div>
       <div className="flex gap-2">
         <img
-          src="/assets/icons/save.svg"
+          src={isSaved ? "/assets/icons/saved.svg" : "/assets/icons/save.svg"}
           alt="save"
           width={20}
           height={20}
-          onClick={() => {}}
+          onClick={handleSavePost}
           className="cursor-pointer"
         />
       </div>
