ca4961d16529ad69be18c12693439faa6513ead9
rebixpl <33410494+rebixpl@users.noreply.github.com>
2023-12-22 00:43:20 +0100

    explore page - grid post list 2

--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -1,6 +1,7 @@
 import { useUserContext } from "@/context/AuthContext";
 import { Models } from "appwrite";
 import { Link } from "react-router-dom";
+import PostStats from "./PostStats";
 
 type GridPostListProps = {
   posts: Models.Document[];
@@ -26,6 +27,23 @@ const GridPostList = ({
               className="h-full w-full object-cover"
             />
           </Link>
+
+          <div className="grid-post_user">
+            {showUser && (
+              <div
+                className="flex items-center 
+                justify-start gap-2 flex-1"
+              >
+                <img
+                  src={post.creator.imageUrl}
+                  alt="creator"
+                  className="h-8 w-8 rounded-full"
+                />
+                <p className="line-clamp-1">{post.creator.name}</p>
+              </div>
+            )}
+            {showStats && <PostStats post={post} userId={user.id} />}
+          </div>
         </li>
       ))}
     </ul>
